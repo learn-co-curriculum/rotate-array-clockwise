fd414551cf0fcc7730c18eb6c2a9de0ee3f0e857
Rupa <rupa211984@gmail.com>
2020-12-02 12:39:33 -0800

    Add arrows

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -5,17 +5,17 @@ function rotateArray(arr, k) {
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [4, 1, 2, 3]");
-  console.log(rotateArray([1, 2, 3, 4], 1));
+  console.log("=>", rotateArray([1, 2, 3, 4], 1));
 
   console.log("");
 
   console.log("Expecting: [2, 3, 1]");
-  console.log(rotateArray([1, 2, 3], 2));
+  console.log("=>", rotateArray([1, 2, 3], 2));
 
   console.log("");
 
   console.log("Expecting: [1, 2, 3]");
-  console.log(rotateArray([1, 2, 3], 3));
+  console.log("=>", rotateArray([1, 2, 3], 3));
 }
 
 module.exports = rotateArray;
--- ruby/rotate_array.rb
+++ ruby/rotate_array.rb
@@ -4,16 +4,19 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: [4, 1, 2, 3]"
+  print "=> "
   print rotate_array([1, 2, 3, 4], 1)
 
   puts
 
   puts "Expecting: [2, 3, 1]"
+  print "=> "
   print rotate_array([1, 2, 3], 2)
 
   puts
 
   puts "Expecting: [1, 2, 3]"
+  print "=> "
   print rotate_array([1, 2, 3], 3)
   # Don't forget to add your own!
 end
