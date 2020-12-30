9a235985fb947077a2b36b3aeb72aa1df8ca2e1c
Rupa <rupa211984@gmail.com>
2020-12-29 16:43:07 -0800

    Modify tests

--- javascript/tests/rotate_array.test.js
+++ javascript/tests/rotate_array.test.js
@@ -1,26 +1,22 @@
 const rotateArray = require('../rotate_array');
 
-function arrayToString(arr) {
-  return JSON.stringify(arr);
-}
-
 test('can handle k values from 1 up to the length of the array', () => {
-  expect(arrayToString(rotateArray([1, 2, 3, 4], 1))).toBe(arrayToString([4, 1, 2, 3]));
-  expect(arrayToString(rotateArray([1, 2, 3], 2))).toBe(arrayToString([2, 3, 1]));
-  expect(arrayToString(rotateArray([1, 2, 3], 3))).toBe(arrayToString([1, 2, 3]));
+  expect(rotateArray([1, 2, 3, 4], 1)).toEqual([4, 1, 2, 3]);
+  expect(rotateArray([1, 2, 3], 2)).toEqual([2, 3, 1]);
+  expect(rotateArray([1, 2, 3], 3)).toEqual([1, 2, 3]);
 });
     
 test('can handle an empty array', () => {
-  expect(arrayToString(rotateArray([], 7))).toBe(arrayToString([]));
-  expect(arrayToString(rotateArray([], 0))).toBe(arrayToString([]));
+  expect(rotateArray([], 7)).toEqual([]);
+  expect(rotateArray([], 0)).toEqual([]);
 });
     
 test('can handle a k value of 0', () => {
-  expect(arrayToString(rotateArray([1, 2, 3], 0))).toBe(arrayToString([1, 2, 3]));
+  expect(rotateArray([1, 2, 3], 0)).toEqual([1, 2, 3]);
 });
     
 test('can handle k values larger than the array length', () => {
-  expect(arrayToString(rotateArray([1, 2, 3], 5))).toBe(arrayToString([2, 3, 1]));
-  expect(arrayToString(rotateArray([1, 2, 3], 6))).toBe(arrayToString([1, 2, 3]));
-  expect(arrayToString(rotateArray([1, 2, 3, 4], 41))).toBe(arrayToString([4, 1, 2, 3]));
+  expect(rotateArray([1, 2, 3], 5)).toEqual([2, 3, 1]);
+  expect(rotateArray([1, 2, 3], 6)).toEqual([1, 2, 3]);
+  expect(rotateArray([1, 2, 3, 4], 41)).toEqual([4, 1, 2, 3]);
 });
